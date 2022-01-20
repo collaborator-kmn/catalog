225314282c01605bfee0fc6bb76160865fe6a81d
collaborator-kmn <68353619+collaborator-kmn@users.noreply.github.com>
2022-01-20 14:18:07 +0300

    init

---
+++ catalog-backend/src/test/java/ogr/template/fullstack/springboottemplate/SpringBootTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package ogr.template.fullstack.springboottemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
